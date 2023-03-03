4d9105e3f6e4151f8e65ec1ead02aa46a05b4067
andrewjarjis <andrewjarjis56@gmail.com>
2023-03-02 21:04:39 -0500

    changed coding

--- NumberAnalyzer.py
+++ NumberAnalyzer.py
@@ -3,7 +3,7 @@ name = input("What is your name? ")
 print("Hello,", name, "! Welcome to our application.")
 
 while True:
-    number = int(input("Please enter an integer between 1 and 100 inclusive, " + name + ": "))
+    number = int(input("Please enter an integer between 1 and 100, " + name + ": "))
     if number < 1 or number > 100:
         print("Invalid input. Please input a positive integer between 1 and 100")
         break
@@ -12,7 +12,7 @@ while True:
     elif number % 2 == 0 and 2 <= number <= 24:
         print(number, "is even and less than 25, " + name + ".")
     elif number % 2 == 0 and 26 <= number <= 60:
-        print(number, "is even and between 26 and 60 inclusive, " + name + ".")
+        print(number, "is even and between 26 and 60, " + name + ".")
     elif number % 2 == 0 and number > 60:
         print(number, "is even and greater than 60, " + name + ".")
     elif number % 2 == 1 and number > 60:
